d9ac0f152177d09851616800d2657822c2c62923
joseilizarbe <jose.ilizarbee@gmail.com>
2026-01-19 13:29:21 -0500

    initial commit

---
+++ src/main/java/com/josebusiness/challenge_screenmatch_frases/ChallengeScreenmatchFrasesApplication.java
@@ -0,0 +1,13 @@
+package com.josebusiness.challenge_screenmatch_frases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeScreenmatchFrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeScreenmatchFrasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josebusiness/challenge_screenmatch_frases/ChallengeScreenmatchFrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josebusiness.challenge_screenmatch_frases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeScreenmatchFrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
