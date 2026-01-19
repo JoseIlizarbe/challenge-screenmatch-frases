5c88869f8eecf8dedf2cc7aae4eb7299b3813137
joseilizarbe <jose.ilizarbee@gmail.com>
2026-01-19 13:30:14 -0500

    create: frase dto

---
+++ src/main/java/com/josebusiness/challenge_screenmatch_frases/dto/FraseDTO.java
@@ -0,0 +1,8 @@
+package com.josebusiness.challenge_screenmatch_frases.dto;
+
+public record FraseDTO(
+        String titulo,
+        String frase,
+        String personaje,
+        String poster) {
+}
